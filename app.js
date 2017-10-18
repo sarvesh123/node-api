ad7be6bf7f701f73e951a76584bb8036c9202daa
Sarvesh Shejwadkar <sarveshshejwadkar@gmail.com>
2017-10-18 13:49:50 +0530

    update port

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(passport.session());
 
 app.use(function(req, res, next) {
     res.header('Access-Control-Allow-Credentials', true);
-    res.header("Access-Control-Allow-Origin", "http://localhost:4200");
+    res.header("Access-Control-Allow-Origin", "http://localhost:3001");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
